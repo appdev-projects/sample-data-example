6df6be14b70711213380e8552ac0e83c7fb5a306
Ian <heratyian@gmail.com>
2023-05-08 18:42:10 +0000

    Add user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.find_or_create_by(email: "ihera2@uillinois.edu", username: "ian") do |u|
+  u.password = "password"
+end
